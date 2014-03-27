cb40035ba36e50f19b9db2bc08ea32130e886fbb
Mark Michaelis <thragor@gmx.net>
2014-03-27 23:36:49 +0100

    Documentation update and toString()

--- parent/client/provider/src/main/java/io/github/mmichaelis/selenium/client/provider/RemoteWebDriverProvider.java
+++ parent/client/provider/src/main/java/io/github/mmichaelis/selenium/client/provider/RemoteWebDriverProvider.java
@@ -21,25 +21,47 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
+import javax.annotation.CheckReturnValue;
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import java.net.URL;
 
+import static com.google.common.base.Objects.toStringHelper;
+import static io.github.mmichaelis.selenium.common.internal.PreconditionMessage.MUST_NOT_BE_NULL;
 import static java.lang.String.format;
+import static java.util.Objects.requireNonNull;
 
 /**
+ * <p>
+ * Provides remote web driver instances with given capabilities.
+ * </p>
+ *
  * @since 2014-03-19.
  */
 public final class RemoteWebDriverProvider extends AbstractWebDriverProvider {
-
+  private static final String P_DESIRED_CAPABILITIES = "desiredCapabilities";
+  private static final String P_URL = "url";
   private final URL url;
   private final Capabilities desiredCapabilities;
 
-  public RemoteWebDriverProvider(@Nonnull final URL url,
+  /**
+   * <p>
+   * Create a provider of remote WebDriver instances.
+   * </p>
+   *
+   * @param url                 URL of the Selenium Server like {@code http://localhost:4444/wd/hub}; if {@code null}
+   *                            it falls back to WebDrivers default handling to determine the server address
+   * @param desiredCapabilities specifies the capabilities you want the started browser to have - like for example
+   *                            which browser should be started.
+   */
+  public RemoteWebDriverProvider(@Nullable final URL url,
                                  @Nonnull final Capabilities desiredCapabilities) {
+    requireNonNull(desiredCapabilities, MUST_NOT_BE_NULL.format(P_DESIRED_CAPABILITIES));
     this.url = url;
     this.desiredCapabilities = desiredCapabilities;
   }
 
+  @CheckReturnValue
   @Override
   @Nonnull
   protected WebDriver instantiateDriver() {
@@ -54,4 +76,12 @@ public final class RemoteWebDriverProvider extends AbstractWebDriverProvider {
     }
   }
 
+  @Override
+  @CheckReturnValue
+  public String toString() {
+    return toStringHelper(this)
+            .add(P_URL, url)
+            .add(P_DESIRED_CAPABILITIES, desiredCapabilities)
+            .toString();
+  }
 }
