ab5deba858bf1748bb7d85580f03b1778457f0cb
Mark Michaelis <thragor@gmx.net>
2014-03-29 23:08:15 +0100

    Testing QuitWebDriverRunnable
    Add additional test for QuitWebDriverRunnable.

--- parent/client/provider/src/test/java/io/github/mmichaelis/selenium/client/provider/internal/QuitWebDriverRunnableTest.java
+++ parent/client/provider/src/test/java/io/github/mmichaelis/selenium/client/provider/internal/QuitWebDriverRunnableTest.java
@@ -16,17 +16,21 @@
 
 package io.github.mmichaelis.selenium.client.provider.internal;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.remote.UnreachableBrowserException;
 
 import java.util.concurrent.Executors;
 
 import static org.apache.commons.lang3.RandomStringUtils.random;
-import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.*;
 
@@ -37,8 +41,15 @@ import static org.mockito.Mockito.*;
  */
 @RunWith(MockitoJUnitRunner.class)
 public class QuitWebDriverRunnableTest {
+  @Rule
+  public final ExpectedException expectedException = ExpectedException.none();
+
   @Mock
   private WebDriver driver;
+  @Mock
+  private UnreachableBrowserException unreachableBrowserException;
+  @Mock
+  private WebDriverException webDriverException;
 
   @Test
   public void quit_webdriver_when_running() throws Exception {
@@ -50,11 +61,30 @@ public class QuitWebDriverRunnableTest {
   }
 
   @Test
+  public void ignore_failure_when_server_is_unreachable_meanwhile() throws Exception {
+    final Runnable runnable = new QuitWebDriverRunnable(driver);
+    final Thread thread = Executors.defaultThreadFactory().newThread(runnable);
+    Mockito.doThrow(unreachableBrowserException).when(driver).quit();
+    thread.start();
+    thread.join();
+    verify(driver, atLeastOnce()).quit();
+  }
+
+  @Test
+  public void ignore_failure_on_any_other_failure_when_quitting() throws Exception {
+    final Runnable runnable = new QuitWebDriverRunnable(driver);
+    final Thread thread = Executors.defaultThreadFactory().newThread(runnable);
+    Mockito.doThrow(webDriverException).when(driver).quit();
+    thread.start();
+    thread.join();
+    verify(driver, atLeastOnce()).quit();
+  }
+
+  @Test
   public void toString_provides_relevant_information() throws Exception {
     final Runnable runnable = new QuitWebDriverRunnable(driver);
     final String someString = random(10);
     when(driver.toString()).thenReturn(someString);
-    assertThat(runnable.toString(),
-            allOf(containsString(someString), containsString(QuitWebDriverRunnable.class.getSimpleName())));
+    assertThat(runnable, hasToString(allOf(containsString(someString), containsString(QuitWebDriverRunnable.class.getSimpleName()))));
   }
 }
